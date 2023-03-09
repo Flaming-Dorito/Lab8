0d4503ee38da72da8d56f23bd2134bdf32b6c576
Flaming-Dorito <rit7011503@gmail.com>
2023-03-08 17:16:39 -0700

    Added test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,31 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    /**
+     * test the addCity method
+     */
+    @Test
+    public void testAddCity(){
+        list = MockCityList();
+        City city = new City("Toronto", "Ontario");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+    }
+
+
+
+
+
 
 }
